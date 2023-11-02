597cffd2445156de5cd0110b682040cbb80c20eb
Sebastian Miasojed <sebastian.miasojed@parity.io>
2023-11-02 23:46:29 +0100

    test 3

--- scripts/contract_sizes_submit.sh
+++ scripts/contract_sizes_submit.sh
@@ -6,6 +6,7 @@
 #   ./contract_sizes_submit.sh \
 #     <github_url_to_comments_of_pr>
 #     <github_url_to_workflow>
+#     <cargo_contract_version>
 #     < <diffs-csv-file>
 
 set -eu
@@ -13,12 +14,13 @@ set -o pipefail
 
 pr_comments_url=$1
 workflow_url=$2
+cc_version=$3
 diffs_markdown_table=$(</dev/stdin)
 
 # If there is already a comment by the user `paritytech-cicd-pr` in the ink! PR which triggered
 # this run, then we can just edit this comment (using `PATCH` instead of `POST`).
 possibly_comment_url=$(curl --silent $pr_comments_url | \
-  jq -r ".[] | select(.user.login == \"paritytech-cicd-pr\" or .user.login == \"github-actions\") | .url" | \
+  jq -r ".[] | select(.user.login == \"paritytech-cicd-pr\" or .user.login == \"xgithub-actions[bot]\") | .url" | \
   head -n1
 )
 echo $possibly_comment_url
