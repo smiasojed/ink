e3643d4d85772abdd5f89c1a49da14abb831915b
paritytech-ci <paritytech-ci@parity.io>
2024-01-15 22:27:20 +0000

    Updated docs for master} and pushed to gh-pages

---
+++ implementors/core/iter/traits/collect/trait.FromIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"ink_storage":[["impl&lt;V, KeyType&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.0/core/iter/traits/collect/trait.FromIterator.html\" title=\"trait core::iter::traits::collect::FromIterator\">FromIterator</a>&lt;V&gt; for <a class=\"struct\" href=\"ink_storage/struct.StorageVec.html\" title=\"struct ink_storage::StorageVec\">StorageVec</a>&lt;V, KeyType&gt;<span class=\"where fmt-newline\">where\n    V: Packed + EncodeLike&lt;V&gt;,\n    KeyType: StorageKey,</span>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
